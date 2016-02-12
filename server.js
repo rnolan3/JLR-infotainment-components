6d4570a27ba07a4493c280f9519f6c2848f397e2
Robert Nolan <robert@vohgo.com>
2016-02-11 21:09:05 -0600

    Fix public build directory
    Remove all the semicolons.

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-console, no-var */
-var express = require('express');
-var webpack = require('webpack');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var WebpackConfig = require('./webpack.config');
+var express = require('express')
+var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var WebpackConfig = require('./webpack.config')
 
-var app = express();
+var app = express()
 
 app.use(webpackDevMiddleware(webpack(WebpackConfig), {
-  publicPath: '/js/',
+  publicPath: '/__build__/',
   stats: {
     colors: true
   }
-}));
+}))
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname))
 app.listen(8080, () =>
-  console.log('Server listening on http://localhost:8080, Ctrl+C to stop'));
+  console.log('Server listening on http://localhost:8080, Ctrl+C to stop'))
