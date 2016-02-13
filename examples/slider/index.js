dccfcb711ba44f383fab4b541ee8d4bcc3f19d28
Robert Nolan <robert@vohgo.com>
2016-02-12 18:13:46 -0600

    Console.log the Slider onChange event

--- examples/slider/index.js
+++ examples/slider/index.js
@@ -1,9 +1,14 @@
+/* eslint no-console: 0 */
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Slider from '../../components/slider/Slider'
 
 import styles from './styles.scss'
+
+function consoleValue (value) {
+  console.log('slider value:', value)
+}
 
 ReactDOM.render(<div>
   <Slider
@@ -15,5 +20,6 @@ ReactDOM.render(<div>
     className={ styles.colRight }
     defaultValue="18"
     direction="right"
-    label="R" />
+    label="R"
+    onChange={ consoleValue } />
 </div>, document.getElementById('app'))
